765471f0883df9a34fad98dcf1381ee5c69ffaa2
Alpha <alpha@pokesplash.net>
2012-01-18 14:42:20 -0500

    Added data structures for *_help and *_set

--- includes/services.h
+++ includes/services.h
@@ -25,7 +25,25 @@ typedef struct _commandnode {
     struct _commandnode *next;
 } commandnode;
 
+typedef struct _helpnode {
+    char *command;
+    char *shorthelp;
+    void (*longhelp)(char *uid, char *msg);
+    struct _helpnode *next;
+} helpnode;
+
+typedef struct _setnode {
+    char *option;
+    char *shorthelp;
+    void (*longhelp)(char *uid, char *msg);
+    void (*callback)(char *uid, char *msg);
+    struct _setnode *next;
+} setnode;
+
 user *createService(char *nick, char *host, char *ident, char *gecos);
+void addHelp(helpnode *list, char *command, char *shorthelp, void (*longhelp)(char *uid, char *msg));
+void addSetOption(setnode *list, char *command, char *shorthelp, void (*longhelp)(char *uid, char *msg));
+
 
 /*typedef struct _serverconn {
     char *host;
