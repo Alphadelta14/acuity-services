be59f68904c32d7d59d2377272edf97a3e6d0a9e
Alpha <alpha@pokesplash.net>
2012-01-28 12:09:03 -0500

    Added ns_listgroups to ns_list

--- src/modules/ns_list.c
+++ src/modules/ns_list.c
@@ -11,7 +11,18 @@ void ns_list(char *uid, char *msg){
         nicks = nicks->next;
     }
 }
+void ns_listgroups(char *uid, char *msg){
+    /* XXX: how can this possibly get alphabetized */
+    nickgrouplist *groups;
+    int count = 1;
+    groups = registerednickgroups;
+    while(groups){
+        ns_message(uid, "%d. \x02%s\x02", count++, groups->group->main->nick);
+        groups = groups->next;
+    }
+}
 
 void INIT_MOD(){
     registerNickServCommand("list", ns_list);
+    registerNickServCommand("listgroups", ns_listgroups;
 }
