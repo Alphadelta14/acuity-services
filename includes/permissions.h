e218bdadd56bfa0f292a3b154f4c46b3c197061d
Alpha <alpha@pokesplash.net>
2012-02-11 14:03:32 -0500

    Completed initial permission system

--- includes/permissions.h
+++ includes/permissions.h
@@ -1,21 +1,13 @@
 #ifndef PERMISSIONS_H
 #define PERMISSIONS_H
 
-#define MTX_PERMTX 0x1
-#define MTX_VALUE  0x2
-
 /* note: PERMTX is easier to read than PERMMTX */
 typedef struct _permtxlist permtxlist;
 
-typedef union _permtxnode {/* maybe the only union in acuity */
-    permtxlist *mtx;/* MTX_PERMTX */
-    int perm;/* MTX_VALUE */
-} permtxnode;
-
 struct _permtxlist {
     char *name;
-    char type;/* MTX_ */
-    permtxnode node;
+    permtxlist *subperm;
+    int perm;
     permtxlist *next;
 };
 
@@ -23,5 +15,14 @@ typedef struct _permclass {
     char *name;
     struct _permclass *parent;
     permtxlist *mtx;
+    struct _permclass *next;
 } permclass;
+
+permclass *getPermissionClass(char *name);
+permclass *addPermissionClass(char *name);
+void setPermissionParent(char *name, char *parentname);
+void addPermission(char *name);
+void setPermission(char *classname, char *permname, int perm);
+int getPermission(char *classname, char *permname);
+
 #endif /* PERMISSIONS_H */
---
+++ src/permission.c
@@ -0,0 +1,149 @@
+#include <services.h>
+#include <permissions.h>
+#include <string.h>
+#include <stdlib.h>
+
+permclass defaultperms = {"DEFAULT", NULL, NULL, NULL};
+permclass *permclasslist = &defaultperms;
+
+static permtxlist *createMTX(char *name){
+    permtxlist *mtx;
+    int len;
+    char *index;
+    safemalloc(mtx, permtxlist, NULL);
+    mtx->subperm = NULL;
+    mtx->perm = 0;
+    mtx->next = NULL;
+    index = strstr(name, ".");
+    if(index){
+        len = (int)(index-(name));
+    } else {
+        len = strlen(name);
+    }
+    safenmalloc(mtx->name, char, len+1, NULL)/* not always static */
+    strncpy(mtx->name, name, len);
+    return mtx;
+}
+
+static void addPermissionToMTX(permtxlist **mtx, char *name){
+    permtxlist *list;
+    char *tail;
+    list = *mtx;
+    int len;
+    tail = strstr(name, ".");
+    if(tail){
+        len = (int)(tail-(name));
+        tail++;
+    } else {
+        len = strlen(name);
+    }
+    if(!list){
+        list = *mtx = createMTX(name);
+        if(tail)
+            addPermissionToMTX(&list->subperm, tail);
+        return;
+    }
+    while(list){
+        if(!strncasecmp(list->name, name, len)){
+            if(tail)
+                addPermissionToMTX(&list->subperm, tail);
+            return;
+        }
+        list = list->next;
+    }
+    list = createMTX(name);
+    list->next = *mtx;
+    *mtx = list;
+    if(tail)
+        addPermissionToMTX(&list->subperm, tail);
+    return;
+}
+
+static permtxlist *getPermissionMTX(permtxlist *mtx, char *name){
+    permtxlist *list;
+    char *tail;
+    list = mtx;
+    int len;
+    tail = strstr(name, ".");
+    if(tail){
+        len = (int)(tail-(name));
+        tail++;
+    } else {
+        len = strlen(name);
+    }
+    while(list){
+        if(!strncasecmp(list->name, name, len)){
+            if(tail)
+                return getPermissionMTX(list->subperm, tail);
+            return list;
+        }
+        list = list->next;
+    }
+    return NULL;
+}
+
+void addPermission(char *name){
+    addPermissionToMTX(&(defaultperms.mtx), name);
+}
+
+permclass *addPermissionClass(char *name){
+    permclass *node;
+    if((node = getPermissionClass(name)))
+        return node;
+    safemalloc(node, permclass, NULL);
+    safenmalloc(node->name, char, strlen(name)+1, NULL);
+    strcpy(node->name, name);
+    node->mtx = NULL;
+    node->parent = &defaultperms;
+    node->next = defaultperms.next;/* front insertion */
+    defaultperms.next = node;
+    return node;
+}
+
+permclass *getPermissionClass(char *name){
+    permclass *node;
+    node = permclasslist;
+    while(node){
+        if(!strcasecmp(name, node->name)){
+            return node;
+        }
+        node = node->next;
+    }
+    return NULL;
+}
+
+void setPermclassParent(char *name, char *parentname){
+    permclass *child, *parent;
+    if(!(child = getPermissionClass(name)))
+        return;
+    if(!(parent = getPermissionClass(parentname)))
+        return;
+    child->parent = parent;
+}
+
+void setPermission(char *classname, char *permname, int perm){
+    permtxlist *mtx;
+    permclass *class;
+    if(!(class = getPermissionClass(classname)))
+        return;
+    addPermissionToMTX(&(class->mtx), permname);
+    mtx = getPermissionMTX(class->mtx, permname);
+    if(!mtx)
+        return;/* er, wha? */
+    mtx->perm = perm;
+}
+
+int getPermission(char *classname, char *permname){
+    permtxlist *mtx;
+    permclass *class;
+    if(!(class = getPermissionClass(classname)))
+        return 0;
+    mtx = getPermissionMTX(class->mtx, permname);
+    if(!mtx){
+        if(class->parent)
+            return getPermission(class->parent->name, permname);
+        else
+            return 0;
+    }
+    return mtx->perm;
+}
