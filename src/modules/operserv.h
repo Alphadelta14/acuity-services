5d0af23e0586f09870f121633d9a7708f6b2323b
Alpha <alpha@pokesplash.net>
2012-02-12 08:29:39 -0500

    Added operserv functions to operserv.h

--- src/modules/operserv.h
+++ src/modules/operserv.h
@@ -4,5 +4,8 @@
 #include "nickserv.h"
 
 extern user *operserv;
+void registerOperServCommand(char *cmd, void (*callback)(char *uid, char *msg));
+void fireOperServCommand(line *l);
+void os_message(char *uid, char *str, ...);
 
 #endif /* OPERSERV_H */
